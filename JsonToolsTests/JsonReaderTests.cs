ebca39fb2bcaadfdaa727e45342e2f3056fe4fa0
Colin Middleton <microsoft@medievalresearch.co.uk>
2023-11-23 20:38:49 +0000

    Add JsonReader Tests outline.

---
+++ JsonToolsTests/JsonReaderTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CM.JsonTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM.JsonTools.Tests
+{
+    [TestClass()]
+    public class JsonReaderTests
+    {
+        [TestMethod()]
+        public void JsonReaderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReadJsonTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void addNodeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
